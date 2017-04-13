f674451eb22ef73f77bb41e066138d70df91e5ea
zhanshen1373 <827168777@qq.com>
2017-04-14 00:24:32 +0800

    updata commit

--- app/src/main/java/com/example/myapplication/Main24Activity.java
+++ app/src/main/java/com/example/myapplication/Main24Activity.java
@@ -3,11 +3,11 @@ package com.example.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Main23Activity extends AppCompatActivity {
+public class Main24Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main23);
+        setContentView(R.layout.activity_main24);
     }
 }
